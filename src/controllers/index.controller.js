ade2dba3823cedff841c07cdbad3ee6b84fee582
jairo457 <20030708@itcelaya.edu.mx>
2024-05-02 11:45:40 -0600

    primero

---
+++ src/controllers/index.controller.js
@@ -0,0 +1,57 @@
+const { Pool } = require('pg');
+
+const pool = new Pool({
+    host: 'localhost',
+    user: 'jairo',
+    password: '123',
+    database: 'firstapi',
+    port: '5432'
+});
+
+
+const getUsers = async (req, res) => { 
+    const response = await pool.query('SELECT * FROM users');
+     res.status(200).json(response.rows);
+}
+
+const getUserById = async (req, res) =>{
+    const id = req.params.id;
+    const response = await pool.query('SELECT * FROM users WHERE id = $1',[id]);
+    res.json(response.rows);
+}
+
+const deleteUser = async (req, res) =>{
+    const id = req.params.id;
+    const response = await pool.query('DELETE FROM users WHERE id = $1',[id]);
+    res.json('User ${id} delete succefully');
+}
+
+const updateUser = async (req, res) =>{
+    const id = req.params.id;
+    const {name, email}  = req.body;
+    const response = await pool.query('UPDATE users SET name = $1, email = $2 WHERE id = $3',
+    [name,email,id]);
+    res.send('User Updated');
+    console.log(response);
+}
+
+const createUser = async (req, res) =>{
+const {name, email}  = req.body;
+const response = await pool.query('INSERT INTO  users (name, email) VALUES ($1, $2)', 
+[name, email])
+console.log(response);
+res.json({
+    message: 'User Added Succesfully',
+    body:{
+        user: {name, email}
+    }
+})
+};
+
+module.exports = {
+    deleteUser,
+    getUserById,
+    getUsers,
+    createUser,
+    updateUser
+}
